467d0ab9afcf083dae52f974d4146f40a8735eed
Léo Serrão Barbosa <leo_serrao@hotmail.com>
2020-08-11 21:55:46 -0300

    Servidor rodando

---
+++ server/app.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+
+class App {
+    public app: express.Application;
+
+    constructor(){
+        this.app = express();
+        this.routes();
+    }
+
+    routes(){
+        this.app.route('/').get( (req,res) => res.status(200).json({"result": "Hello World"}) );
+    }
+}
+
+export default new App();
